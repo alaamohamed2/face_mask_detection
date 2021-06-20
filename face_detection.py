39bfb0656e7708bb81949a5a845c9a4e32ffd59a
Mai2207 <65425224+Mai2207@users.noreply.github.com>
2021-06-20 02:53:43 +0200

    Add files via upload

---
+++ face_detection.py
@@ -0,0 +1,30 @@
+import cv2
+import os
+import mediapipe as mp
+
+# Color values in BGR
+RED = (0, 0, 255)
+GREEN = (0, 255, 0)
+
+padding =  70
+
+
+
+def print_face(image, face, is_masked):
+    """ Prints face with color corresponding to if face is masked or not.
+
+    Args:
+        image (int[]): Array of image pixels.
+        face (tuple): Bounds of face.
+        is_masked (boolean): True if mask, false otherwise.
+
+    Returns:
+        int[]: Colored image of face with or without mask.
+    """
+    x, y, w, h = face
+    if is_masked:
+        image = cv2.rectangle(image, (x - padding // 2, y - padding // 2), (x + w + padding // 2, y + h + padding // 2), GREEN)
+    else:
+        image = cv2.rectangle(image, (x - padding // 2, y - padding // 2), (x + w + padding // 2, y + h + padding // 2), RED)
+
+    return image
